d8c9345f9b031ffcf348f323fcff82abee9ba8ce
Neeraj Shetkar <neerajshetkar@gmail.com>
2023-12-08 16:14:07 +0530

    Initial Commit

---
+++ chemAR/chemARApp.swift
@@ -0,0 +1,17 @@
+//
+//  chemARApp.swift
+//  chemAR
+//
+//  Created by Neeraj Shetkar on 08/12/23.
+//
+
+import SwiftUI
+
+@main
+struct chemARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
